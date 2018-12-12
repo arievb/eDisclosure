7f69530f8f87a24c6ef749a1594f4623d55810c5
arievb <45426920+arievb@users.noreply.github.com>
2018-12-12 13:52:56 +0200

    resizing

--- scripts/resize.js
+++ scripts/resize.js
@@ -3,10 +3,10 @@ function ResizeContent() {
   var divMenu = document.getElementById('divMenu');
   var divHeader = document.getElementById('divHeader');
   var divFooter = document.getElementById('divFooter');
-  outersize = divMenu.clientHeight + divHeader.clientHeight + divFooter.clientHeight + (window.innerHeight - window.outerHeight);
+  outersize = divMenu.clientHeight + divHeader.clientHeight + divFooter.clientHeight + 3;
   divContent.style.setProperty('height', window.innerHeight - outersize - 16 + 'px');
 }
  
 window.onresize = function() {ResizeContent();};
-alert(window.innerHeight - window.outerHeight);
+//alert(window.innerHeight - window.outerHeight);
 
